d7f0ca38e4a7170307ab00a1f703754e4b3c829d
Amy Angert <amyangert@amys-mbp-3.lan>
2020-12-14 10:25:26 -0800

    Redid bioclimatic variable selection

--- SDM/Amy's_SDM_scripts/3_PredictorSelection.R
+++ SDM/Amy's_SDM_scripts/3_PredictorSelection.R
@@ -18,7 +18,7 @@
 rm(list = ls(all.names = TRUE))
 
 ## LIBRARIES
-library(tidyverse)
+library(tidyverse) # for gpplot and data manipulations
 library(GGally) # for visualizing correlation matrix
 
 ## INPUTS
@@ -48,7 +48,7 @@ cor.dat <- dat %>%
 
 # scatterplot matrix with univariate distribution of each variable along diagonal
 cor.mat <- ggpairs(cor.dat)
-cor.mat
+cor.mat # view this in conjunction with variable importance, below
 
 ################################################################################
 
@@ -100,7 +100,7 @@ dev.fit = dev.fit[order(dev.fit$AIC),]
 dev.fit 
 
 # basic barplot of explanatory power
-ggplot(dev.fit, aes(x=Var, y=AdjDev)) + geom_bar(stat="identity")
+#ggplot(dev.fit, aes(x=Var, y=AdjDev)) + geom_bar(stat="identity")
 
 ################################################################################
 
@@ -110,28 +110,63 @@ ggplot(dev.fit, aes(x=Var, y=AdjDev)) + geom_bar(stat="identity")
 
 ## (NOTE: variable selection is altered from Angert et al. 2018, due to addition of occupancy dataset to training input &/or use of 1961-1990 normals)
 
-# Single best variable = bio15
-# bio15 has no |r|>0.7 with other bioclim
-# 2nd best variable = bio10
-# If bio10 is included, then bio1, bio5, bio8, and bio9 should be excluded (|r|>0.7)
-# Next best variable of those remaining = bio14
-# If bio14 is included, then bio17 and bio18 should be excluded (|r|>0.7)
-# Next best bariable = bio12
-# If bio12 is included, then bio13, bio16, and bio19 should be excluded (|r|>0.7)
-# Next best variable = bio11
-# If bio11 is included, then bio1, bio6, and bio8 should be excluded (|r|>0.7)
-# Next best variable = bio4
-# If bio4 is included, then bio7 should be excluded (|r|>0.7)
-# Next best of remaining variables = bio3
-# bio3 has no |r|>0.7 with other bioclim
-# Choose last remaining = bio2
+# Single best variable = bio10. INCLUDE
+# bio10 has |r|>0.7 with bio1, bio5, bio8, bio9, bio11
+# If bio10 is included, then bio1, bio5, bio8, bio9, and bio11 should be excluded (|r|>0.7)
 
-## Final list of variables to include:
-	#bio15, bio10, bio14, bio12, bio11, bio4, bio3, bio2
+# 2nd best variable = bio1, but that is collinear with bio10. EXCLUDE
+
+# 3rd best variable = bio5, but that is collinear with bio10. EXCLUDE
+
+# 4th best variable = bio14. INCLUDE
+# bio14 has |r|>0.7 with bio1, bio17, bio18
+# If bio14 is included, then bio1, bio17 and bio18 should be excluded (|r|>0.7)
+
+# 5th best variable = bio11, but that is collinear with bio10. EXCLUDE
+
+# 6th best variable = bio15. INCLUDE
+# bio15 is not too collinear with any other variable.
+
+# 7th best variable = bio9, but that is collinear with bio10. EXCLUDE
+
+# 8th best variable = bio8, but that is collinear with bio10. EXCLUDE
+
+# 9th best variable = bio17, but that is collinear with bio14. EXCLUDE
+
+# 10th best variable = bio18, but that is collinear with bio14. EXCLUDE.
+
+# 11th best variable = bio12. INCLUDE
+# bio12 has |r|>0.7 with bio13, bio16, bio19
+# If bio12 is included, then bio13, bio16 and bio19 should be excluded (|r|>0.7)
+
+# 12th best variable = bio6. INCLUDE
+# bio6 has |r|>0.7 with bio1, bio8, bio11 but these have already been excluded
+
+# 13th best variable = bio16, but this is collinear with bio12. EXCLUDE
+
+# 14th best variable = bio13, but this is collinear with bio12. EXCLUDE
+
+# 15th best variable = bio19, but this is collinear with bio12. EXCLUDE
+
+# 16th best variable = bio3. INCLUDE
+# bio3 has |r|>0.7 with bio4
+# If bio3 is included, then bio4 should be excluded (|r|>0.7)
+
+# 17th best variable = bio2
+# bio2 has |r|>0.7 with bio7
+# If bio2 is included, then bio7 should be excluded (|r|>0.7)
+
+# 18th best variable = bio4, but this is collinear with bio3. EXCLUDE
+
+# last variable = bio7, but this is collinear with bio2
+
+## Final list of variables to INCLUDE: bio10, bio14, bio15, bio12, bio6, bio3 bio2
+
+## Final list of variables to EXCLUDE: bio1, bio5, bio8, bio9, bio11, bio17, bio18, bio13, bio16, bio19, bio4, bio7
 
 ## Save file with selected predictors
 dat.input <- dat %>% 
-  select(set, lat, long, el, presabs, bio15, bio10, bio14, bio12, bio11, bio4, bio3, bio2)
+  dplyr::select(Master.ID, Latitude, Longitude, Elevation, presabs, bio10, bio14, bio15, bio12, bio6, bio3, bio2)
 write_csv(dat.input, "SDM/data_files/sdm_input.csv")
 
 ################################################################################
