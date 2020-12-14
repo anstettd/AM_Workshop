f59ef6539046de32909bfed4a70f2ae77bdef6c6
Amy Angert <amyangert@amys-mbp-3.lan>
2020-12-14 05:51:59 -0800

    yesterday's edits

--- SDM/Amy's_SDM_scripts/4_LogisticRegression.r
+++ SDM/Amy's_SDM_scripts/4_LogisticRegression.r
@@ -1,140 +1,66 @@
+################################################################################# 
+### SCRIPT PURPOSE: SDM based on generalized linear model (i.e., logistic regression of presence/absence ~ bioclimatic predictors)
+# Modified from Angert et al. 2018, American Naturalist
+# Author: Amy Angert
+# last update:  13 Dec 2020
+
+## OVERALL WORKFLOW:
+  #	Build full and reduced model logistic GLMs
+  #	Perform resubstitution and cross-validation to assess accuracy
+  #	Calculate Miller's calibration statistics to assess reliability
+
 ###################################################################################
-## Amy Angert
-## Modified from Thomas C. Edwards, Jr., "Species Distribution Modelling Using R"
-## PROGRAM FUNCTIONS: 
-##   For each of 10 replicate training data sets (thinned herbarium presences + matching pseudoabsences)
-##		Build full and reduced model logistic GLMs
-##   	Perform resubstitution, cross-validation, and external validation to assess accuracy
-##		Calculate Miller's calibration statistics to assess reliability
-## last update:  22 April 2016 (Amy going through all code to triple-check for errors and make final figures and tables)
-###################################################################################
 
-## load libraries now if desired; loaded below when needed
-#library(PresenceAbsence)
-#library(DAAG)
 
-## set pathnames - Amy Angert
-#path.root="/Users/amylauren/Google Drive/Occupancy AmNat Amy" 
-#path.dat=paste(path.root, "/data files", sep="")
-#path.obj=paste(path.root, "/R objects", sep="")
-#path.fig=paste(path.root, "/figures", sep="")
-#path.cod=paste(path.root, "/R code", sep="")
+################################################################################# 
+### LOAD LIBRARIES AND PREPARE INPUTS
 
-## set pathnames - Matthew
-#path.root = "C:/Users/DW/Desktop/temp.sept.30" 
-#path.dat = paste(path.root, "/data files", sep="")
-#path.dat.fix = paste(path.root, "/data files", sep="") # older files relocated to another directory
-#path.obj = paste(path.root, "/R objects", sep="")
-#path.eco = paste(path.obj, "/ecoregions.shp", sep="")
-#path.bio = paste(path.obj, "/wc0.5", sep="")
-#path.cod=paste(path.root, "/R code", sep="")
-#path.fig=paste(path.root, "/figures", sep="")
+## LIBRARIES
+library(PresenceAbsence)
+library(DAAG)
 
+## INPUTS
+# read in file
+dat <- read_csv('SDM/data_files/sdm_input.csv')
+
+# if necessary, filter rows to appropriate ratio of pseudoabsences:presences 
+# (not necessary here; all rows are used in GLM)
+
+# slim dataframe to conform to structure required by mod.form function
+# (see script modforms.R)
+dat.input <- dat %>% 
+  select(presabs, bio15, bio10, bio14, bio12, bio11, bio4, bio3, bio2)
 
 ################################################################################
-######## START INITIALIZATION
-## import data
-## slim down variables to conform to structure required by mod.form function	
-## variables chosen based on dev and collinearity: bio15, lnbio10, lnbio14, lnbio12, bio11, bio4, lnbio3, bio2
-## variables requiring ln-transform are already transformed (see RCode_ThinPseudos)
+### BUILD FULL AND REDUCED MODELS 
 
-#setwd(path.dat) 
+## Source code file with function for models
+source("SDM/Amy's_SDM_scripts/modforms.R")
 
-#for (i in 1:10) {
-#	dat = read.csv(paste("dat",i,".csv", sep=""))
-#	dat = dat[,c(4,59:61,67:69,71:72)] #column indices changed 9/4/14
-#	dat = na.omit(dat) 
-#	assign(paste("dat",i, sep=""), dat)
-#	}
-#rm(dat)
+## Build initial model with all variables: mod1.LR
+mod1.LR <- glm(mod.form.quad(dat, 1, 2), family=binomial, data=dat)
+#mod1.fit=100*(1-mod1.LR$deviance/mod1.LR$null.deviance)
+mod1.pred <- predict(mod1.LR, type="response") # model prediction
+#summary(mod1.LR) # full model summary stats
+save(mod1.LR, file="SDM/Output/LR.mod1.Rda") # save model object
 
-#for (i in 1:10) {
-#	dat = read.csv(paste("dat",i,"b.csv", sep=""))
-#	dat = dat[,c(4,59:61,67:69,71:72)] #column indices changed 9/4/14
-#	dat = na.omit(dat) 
-#	assign(paste("dat",i, sep=""), dat)
-#	}
-#rm(dat)
+## Build most parsimonious model using backwards variable reduction: mod2.LR
+mod2.LR <- step(mod1.LR, trace=F) # backwards stepwise variable reduction
+#mod2.fit=100*(1-mod2.LR$deviance/mod2.LR$null.deviance) # model fit
+mod2.pred <- predict(mod2.LR, type="response") # model prediction
+#summary(mod2.LR)  # reduced model summary	
+save(mod2.LR, file="SDM/Output/LR.mod2.Rda") # save model object
 
-## use these (1:1 ratio)
-for (i in 1:10) {
-	dat = read.csv(paste("SDM/Output/dat",i,"c.csv", sep=""))
-	dat = dat[,c(4,59:61,67:69,71:72)] #column indices changed 9/4/14
-	assign(paste("dat",i, sep=""), dat)
-	}
-rm(dat)
-
-## examine training data
-dim(dat1); dim(dat10)
-table(dat1$PRESABS); table(dat10$PRESABS)     
-head(dat1); head(dat10)                     
-str(dat1); str(dat10)                      
-
-## testing data
-all = read.csv("SDM/data_files/all.records.aug.31.csv") #includes occupancy dataset, cleaned herbarium records, and 20K pseudoabs drawn to match envir space of true absences
-ext = all[all$DATASET=="occ",] #pull out occupancy dataset
-ext$bio3 = log(ext$bio3+0.5) #make needed ln-transforms of predictors
-ext$bio10 = log(ext$bio10+0.5)
-ext$bio12 = log(ext$bio12+0.5)
-ext$bio14 = log(ext$bio14+0.5)
-
-######## END INITIALIZATION
 ################################################################################
 
 
+################################################################################
+### {IF NECESSARY} LOAD FINAL REDUCED MODELS AND THEIR PREDICTIONS 
 
+mod = get(load("SDM/Output/LR.mod2.Rda"))
+pred = predict(mod, type="response") # model prediction
 
 ################################################################################
-######## {DO NOT RERUN} START BUILD FULL AND REDUCED MODELS 
-
-#setwd(path.cod)
-source("SDM/R_code/modforms.R")
-
-#setwd(path.obj)
-
-for (i in 1:10) {
-	## build initial model all variables: mod1.i.LR
-	dat = get(paste("dat",i, sep=""))
-	mod1.LR=glm(mod.form.quad(dat,1,2),family=binomial,data=dat)
-	assign(paste("LR.mod1.",i, sep=""), mod1.LR)
-	#mod1.fit=100*(1-mod1.LR$deviance/mod1.LR$null.deviance)
-	mod1.pred=predict(mod1.LR,type="response") # model prediction
-	assign(paste("LR.mod1.",i,".pred", sep=""), mod1.pred)
-	#summary(mod1.LR)        # full model summary stats
-	save(mod1.LR, file=paste("SDM/Output/LR.mod1.",i,".pseudo11.Rda", sep="")) # save model object
-
-	## build parsimonious model w/ backwards variable reduction: mod2.i.LR
-	mod2.LR=step(mod1.LR,trace=F) # backwards stepwise variable reduction
-	assign(paste("LR.mod2.",i, sep=""), mod2.LR)
-	#mod2.fit=100*(1-mod2.LR$deviance/mod2.LR$null.deviance) # model fit
-	mod2.pred=predict(mod2.LR,type="response") # model prediction
-	assign(paste("LR.mod2.",i,".pred", sep=""), mod2.pred)
-	#summary(mod2.LR)  # reduced model summary	
-	save(mod2.LR, file=paste("SDM/Output/LR.mod2.",i,".pseudo11.Rda", sep="")) # save model object
-	}
-
-######## END BUILD FULL AND REDUCED MODELS
-################################################################################
-
-
-
-
-################################################################################
-######## {START HERE} LOAD FINAL REDUCED MODELS AND THEIR PREDICTIONS 
-
-#setwd(path.obj)
-
-for (i in 1:10) {
-	mod = get(load(paste("SDM/Output/LR.mod2.",i,".pseudo11.Rda", sep="")))
-	assign(paste("LR.mod2.",i, sep=""), mod)
-	pred=predict(mod,type="response") # model prediction
-	assign(paste("LR.mod2.",i,".pred", sep=""), pred)
-	}
-
-######## LOAD FINAL REDUCED MODELS AND THEIR PREDICTIONS
-################################################################################
-
-
 
 
 ################################################################################
