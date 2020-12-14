83583d739e24fab649b010d8fa21b5551b8b41de
Amy Angert <amyangert@amys-mbp-3.lan>
2020-12-14 10:47:25 -0800

    GAM script and outputs done

--- SDM/Amy's_SDM_scripts/5_GeneralizedAdditive.r
+++ SDM/Amy's_SDM_scripts/5_GeneralizedAdditive.r
@@ -26,7 +26,7 @@ dat <- read_csv('SDM/data_files/sdm_input.csv')
 # slim dataframe to conform to structure required by mod.form function
 # (see script modforms.R)
 dat.input <- dat %>% 
-  select(presabs, bio15, bio10, bio14, bio12, bio11, bio4, bio3, bio2)
+  select(presabs, bio10, bio14, bio15, bio12, bio6, bio3, bio2)
 
 ################################################################################
 
@@ -35,27 +35,28 @@ dat.input <- dat %>%
 ### BUILD FULL AND REDUCED GAM MODELS 
 
 ## Source code file with function for generating lists of model terms
-source("SDM/R_Code/modforms.R")
+source("SDM/Amy's_SDM_scripts/modforms.R")
 
 ## GAM model 1: all smoothers using default df (smoothers=4)
-mod1.GAM <- gam(mod.form.4(dat.input, 1, 2), family=binomial, data=dat.input)
+mod1.GAM <- gam(mod.form.4(dat.input, 1, 2), family=binomial, data=dat.input) #error about non-list constrasts argument can be ignored
 #mod1.fit <- 100*(1-mod1.GAM$deviance/mod1.GAM$null.deviance) 
+#summary(mod1.GAM)
 
 ## GAM model 2: stepwise with different scopes
 # scope list gives range of possible smoothing degrees for each variable
 # Daniel: Changed "step.gam" to "step.Gam" but Amy changed back
 # Amy changing to mod1 as basis and deleting old basis mod2 (smoothers=3)
-mod2.GAM <- step.gam(mod1.GAM, scope=list( 
+mod2.GAM <- step.Gam(mod1.GAM, scope=list( 
 		"bio2"	=~1+ bio2	+s(bio2,2) +s(bio2,3)	+s(bio2,4),
 		"bio3"	=~1+ bio3 	+s(bio3,2) 	+s(bio3,3) 	+s(bio3,4),
-		"bio4"	=~1+ bio4 	+s(bio4,2) 	+s(bio4,3) 	+s(bio4,4),
+		"bio6"	=~1+ bio6 	+s(bio6,2) 	+s(bio6,3) 	+s(bio6,4),
 		"bio10"	=~1+ bio10 	+s(bio10,2) +s(bio10,3) +s(bio10,4),
-		"bio11"	=~1+ bio11 	+s(bio11,2) +s(bio11,3) +s(bio11,4),
 		"bio12"	=~1+ bio12 	+s(bio12,2) +s(bio12,3)	+s(bio12,4),
 		"bio14"	=~1+ bio14 	+s(bio14,2) +s(bio14,3)	+s(bio14,4),
 		"bio15"	=~1+ bio15 	+s(bio15,2) +s(bio15,3)	+s(bio15,4)),
 		trace=F)
-#mod4.fit <- 100*(1-mod4.GAM$deviance/mod4.GAM$null.deviance)  
+#mod4.fit <- 100*(1-mod2.GAM$deviance/mod2.GAM$null.deviance)  
+#summary(mod2.GAM)
 save(mod2.GAM, file="SDM/Output/GAM.mod2.Rda")
 
 ################################################################################
@@ -73,7 +74,7 @@ pred = predict(mod, type="response")
 ################################################################################
 ### ACCURACY CALCULATIONS
 
-source("SDM/R_code/accuracy.R")
+source("SDM/Amy's_SDM_scripts/accuracy.R")
 
 modl="mod2.GAM" # label to keep track of which model
 
