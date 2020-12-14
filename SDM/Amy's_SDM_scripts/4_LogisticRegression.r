3fdd701bee8d2e14e896beb3eb30238a474b6e0d
Amy Angert <amyangert@amys-mbp-3.lan>
2020-12-14 10:39:32 -0800

    GLM script and outputs done

--- SDM/Amy's_SDM_scripts/4_LogisticRegression.r
+++ SDM/Amy's_SDM_scripts/4_LogisticRegression.r
@@ -25,13 +25,10 @@ library(DAAG) # for cross-validation sampling
 # read in file
 dat <- read_csv('SDM/data_files/sdm_input.csv')
 
-# if necessary, filter rows to appropriate ratio of pseudoabsences:presences 
-# (not necessary here; all rows are used in GLM)
-
 # slim dataframe to conform to structure required by mod.form function
 # (see script modforms.R)
 dat.input <- dat %>% 
-  select(presabs, bio15, bio10, bio14, bio12, bio11, bio4, bio3, bio2)
+  select(presabs, bio10, bio14, bio15, bio12, bio6, bio3, bio2)
 
 ################################################################################
 ### BUILD FULL AND REDUCED GLM MODELS 
@@ -68,23 +65,23 @@ pred <- predict(mod, type="response") # model prediction
 ################################################################################
 ### ACCURACY CALCULATIONS
 
-source("SDM/R_code/accuracy.R")
+source("SDM/Amy's_SDM_scripts/accuracy.R")
 
-modl="mod2.GLM"  # label for the model, tracking purposes only
+modl="mod2.GLM"  # label for the model, needed for accuracy function below
 
 ## METHOD 1: resusbtitution
 ## i.e., use full input dataframe as testing dataframes for accuracy calculation 
 acc <- accuracy(dat.input, pred, modl)
 acc$thresh = c("SensSpec", "Kappa")
 acc$model = "GLM.mod2"
-save(accs, file="SDM/Output/GLM.mod2.accs.Rda")
+save(acc, file="SDM/Output/GLM.mod2.accs.Rda")
 
 ## METHOD 2: k-fold cross-validation
 ## i.e., hold out 1 of k folds as testing dataframe for accuracy calculation
 ## (results will vary slightly if re-run due to random division of data set into folds)
 
-cv.accs <- cv.accuracy(mod, dat, modl)
-cv.accs = c("SensSpec", "Kappa")
+cv.accs <- cv.accuracy(mod, dat.input, modl)
+cv.accs$thresh = c("SensSpec", "Kappa")
 cv.accs$model = "GLM.mod2"
 save(cv.accs, file="SDM/Output/GLM.mod2.cvaccs.Rda")
 
