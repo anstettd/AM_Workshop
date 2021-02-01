527cb2a9f478e2845d11afb7ca861af5c4ff6d28
anstettd <daniel_anstett@yahoo.ca>
2021-02-01 12:52:12 -0800

    Genomics_scripts

---
+++ Climate/clip.R
@@ -0,0 +1,17 @@
+# GIS with R Webinar
+library(sf)
+library(tmap)
+library(tidyverse)
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+
+
+calo <- states %>%
+  filter(name_en=="Oregon" |
+           name_en=="California")
+
+
+#plot selected states, by wes bbox
+tm_shape(calo) +
+  tm_borders()
