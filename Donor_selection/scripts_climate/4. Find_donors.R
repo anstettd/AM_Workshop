74b2b00623db4e2deab51418bc7c56896316674c
danstett <daniel_anstett@yahoo.ca>
2021-02-11 11:03:33 -0800

    S15 0.5 maps

--- Donor_selection/scripts_climate/4. Find_donors.R
+++ Donor_selection/scripts_climate/4. Find_donors.R
@@ -15,20 +15,65 @@ rm(list = ls(all.names = TRUE))
 
 ## LIBRARIES
 library(tidyverse) # for data manipulation
-library(rgdal) # for transforming projections
 library(raster) # for handeling rasters
+library(cowplot)
 
 ## INPUTS
 
 #Data
 gcc.clim <- read_csv("Donor_selection/data/gcc.clim.csv")
 # slim dataframe to just bio10 (average temp of warmest quarter)
-gcc.clim <- gcc.clim %>% dplyr::select(GCM, ID1, ID2, Latitude, Longitude, Elevation, 
-                         bio10)
+gcc.clim <- gcc.clim %>% dplyr::select(GCM, ID1, ID2, Latitude, Longitude, Elevation, bio10)
 
-#Rasters
-bio10.6190<-raster("Donor_selection/data/bio10.grd") #bring in 1961 to 1990 bio10 raster
-SDM<-raster("SDM/Output/UnweightedEnsemble_Unprojected.grd") #bring in ensemble SDM raster
+
+
+gcc.clim.S15<-gcc.clim %>% filter(ID1=="S15")
+
+#Climate Raster
+bio10<-raster("Donor_selection/data/bio10.clip.grd") #bring in 1961 to 1990 bio10 raster
+plot(bio10, main="Bio 10 Across Range")
+
+bio10.S15.rcp45.2055 <- bio10
+bio10.S15.rcp45.2055[bio10<(gcc.clim.S15$bio10[1]-0.5)] <- NA
+bio10.S15.rcp45.2055[bio10>(gcc.clim.S15$bio10[1]+0.5)] <- NA
+plot(bio10.S15.rcp45.2055, main="2055 at RCP 4.5")
+
+bio10.S15.rcp45.2085 <- bio10
+bio10.S15.rcp45.2085[bio10<(gcc.clim.S15$bio10[2]-0.5)] <- NA
+bio10.S15.rcp45.2085[bio10>(gcc.clim.S15$bio10[2]+0.5)] <- NA
+plot(bio10.S15.rcp45.2085, main="2085 at RCP 4.5")
+
+bio10.S15.rcp85.2055 <- bio10
+bio10.S15.rcp85.2055[bio10<(gcc.clim.S15$bio10[3]-0.5)] <- NA
+bio10.S15.rcp85.2055[bio10>(gcc.clim.S15$bio10[3]+0.5)] <- NA
+plot(bio10.S15.rcp85.2055, main="2055 at RCP 8.5")
+
+bio10.S15.rcp85.2085 <- bio10
+bio10.S15.rcp85.2085[bio10<(gcc.clim.S15$bio10[4]-0.5)] <- NA
+bio10.S15.rcp85.2085[bio10>(gcc.clim.S15$bio10[4]+0.5)] <- NA
+plot(bio10.S15.rcp85.2085, main="2085 at RCP 8.5")
+
+
+
+
+
+
+
+
+
+#Sample without referencing
+bio10.S15.rcp45.2055 <- bio10
+bio10.S15.rcp45.2055[bio10<(20.35-1)] <- NA
+bio10.S15.rcp45.2055[bio10>(20.35+1)] <- NA
+plot(bio10.S15.rcp45.2055)
+
+bio10.S15.rcp45.2055 <- bio10
+bio10.S15.rcp45.2055[bio10<(20.35-0.5)] <- NA
+bio10.S15.rcp45.2055[bio10>(20.35+0.5)] <- NA
+plot(bio10.S15.rcp45.2055)
+
+
+
 
 
 
