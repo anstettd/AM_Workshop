fcaafdf0904945a7162a6d82eaf761bfa6cde20f
Anstett <anstett3@msu.edu>
2022-07-29 21:33:32 -0400

    offset fix

--- Genomics_scripts/3.3_gradient_forest.R
+++ Genomics_scripts/3.3_gradient_forest.R
@@ -6,25 +6,27 @@
 ## Last Modified Dec 13, 2021
 ###################################################################################
 
+#Library install and import
+
 # Clear environment
 rm(list = ls())
 
 # Get this package retrieving function
 ## This function will automatically load packages that you already have
 ## and will install packages you don't yet have then load them
-ipak <- function(pkg){
+#ipak <- function(pkg){
   # Function written by Dr. Evan Fricke
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = T)
-  sapply(pkg, require, character.only = T)
-}
+#  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+#  if (length(new.pkg)) 
+#    install.packages(new.pkg, dependencies = T)
+#  sapply(pkg, require, character.only = T)
+#}
 
 # Define the packages that the script needs
-myPackages <- c("randomForest", "extendedForest", "gradientForest","tidyverse","raster")
+#myPackages <- c("randomForest", "extendedForest", "gradientForest","tidyverse","raster")
 
 # Load the packages
-ipak(myPackages)
+#ipak(myPackages)
 
 # Install packages
 #install.packages("extendedForest", repos="http://R-Forge.R-project.org")
@@ -37,112 +39,14 @@ library(dismo) # for biovars function
 library(raster) # for raster grids
 library(rgdal) # for transforming projections
 library(sf)
-
-###################################################################################
-
-##Import Data
-snp_clim_bf10 <- read_csv("Genomics_scripts/Data/snp_clim_bay.csv") #pop data
-#env_wna <- read_csv("Genomics_scripts/Data/env_wna.csv") #grided WNA climate data
-
-#snp_clim_bf10NA <- snp_clim_bf10 %>%
-#  select_if(~ !any(is.na(.)))
-####snp_clim_ful <- read_csv("Genomics_scripts/Data/snp_clim_full.csv") # full data
+library(vegan)
+library(gdm)
 
 
-## Generate specific dataframes for GF model
-env_site <- snp_clim_bf10 %>% dplyr::select(MAT,MAP,PAS,EXT,CMD,Tave_wt,Tave_sm,PPT_wt,PPT_sm)
-
-#Computationally intensive, so just load resulting filtered file
-#test_snp <- snp_clim_bf10[,15:114] %>%
-#  select_if(~ !any(is.na(.)))
-#write_csv(test_snp,"Genomics_scripts/Data/test_snp.csv")
-
-#Import filtered test snp datatset
-test_snp <- read.csv("Genomics_scripts/Data/test_snp.csv")
-
-env_site<-as.data.frame(env_site)
-test_snp<-as.data.frame(test_snp)
-
-df_in_1<-data.frame(env_site, test_snp)
-pred<-colnames(env_site)
-resp<-colnames(test_snp)
-
-#resp<-as.factor(resp)
-#resp<-droplevels(resp)
-
-
-###################################################################################
-#Make a climatic 
-##Import 1981-2010 raster data for West NA
-MAT.clip <- raster("Donor_selection/data/clip/MAT.clip.grd")
-MAP.clip <- raster("Donor_selection/data/clip/MAP.clip.grd")
-PAS.clip <- raster("Donor_selection/data/clip/PAS.clip.grd")
-EXT.clip <- raster("Donor_selection/data/clip/EXT.clip.grd")
-CMD.clip <- raster("Donor_selection/data/clip/CMD.clip.grd")
-
-#Seasonal
-PPT_sm.clip <- raster("Donor_selection/data/clip/PPT_sm.clip.grd")
-PPT_wt.clip <- raster("Donor_selection/data/clip/PPT_wt.clip.grd")
-Tave_sm.clip <- raster("Donor_selection/data/clip/Tave_sm.clip.grd")
-Tave_wt.clip <- raster("Donor_selection/data/clip/Tave_wt.clip.grd")
-
-#Stack Raster
-env_wna <- stack(list(MAT=MAT.clip,MAP=MAP.clip,PAS=PAS.clip,EXT=EXT.clip,CMD=CMD.clip,
-                      PPT_sm=PPT_sm.clip,PPT_wt=PPT_wt.clip,Tave_sm=Tave_sm.clip,Tave_wt=Tave_wt.clip))
-#env_wna <- as.data.frame(env_wna, xy=TRUE)
-
-#colnames(env_wna)[1]<-"X"
-#colnames(env_wna)[2]<-"Y"
-
-# The rasters are for all of North America and too large for storage in this repo
-# Trim them to study area and save only trimmed files in the repo
-# Define extent as 1 degree beyond lat-long extent of points
-
-clim <- read_csv("SDM/data_files/points_Normal_1961_1990MSY.csv")
-prj.laea <- "+proj=laea +lon_0=-100 +lat_0=45 +type=crs" ## set laea CRS
-
-
-
-ext <- extent(min(clim$Longitude)-1, max(clim$Longitude)+1, min(clim$Latitude)-1, max(clim$Latitude)+1)
-bbox = as(ext, "SpatialPolygons") #convert coordinates to a bounding box
-prj.wgs = "+proj=longlat + type=crs" #define unprojected coordinate system
-proj4string(bbox) <- CRS(prj.wgs) #set projection
-bbox.lcc = spTransform(bbox, CRS=CRS(prj.laea)) #re-project to match rasters
-
-#Now we're going to use the bbox info as the y argument to extract info from the raster
-
-
-env_wna_df <- extract(env_wna,)
-
-extract(env_wna, bbox.lcc,cellnumbers=T)
-
-###################################################################################
-
-
-gfRef <- gradientForest(cbind(envGF, SNPs_ref), predictor.vars=colnames(envGF),
-                        response.vars=colnames(SNPs_ref), ntree=500, 
-                        maxLevel=maxLevel, trace=T, corr.threshold=0.50)
-
-
-# Gradient Forest Model
-gf <- gradientForest(df_in_1,
-                      predictor.vars = pred, response.vars = resp,
-                      ntree = 500, transform = NULL, compact = T,
-                      nbin = 201 , corr.threshold = 0.5)
-
-
-gf <- gradientForest(cbind(env_site, test_snp),predictor.vars = colnames(env_site),
-                     response.vars = colnames(test_snp),ntree = 500,
-                     maxLevel=maxLevel, trace=T, corr.threshold=0.50)                    
-
-#Importance Plot
-plot(gf)
-
-
-##################################################################################################################
-
-# Mapping spatial genetic variation --------------------------------------------
+#############################################################################################
 ###### functions to support mapping #####
+#Function 1
+
 # builds RGB raster from transformed environment
 # snpPreds = dataframe of transformed variables from gf or gdm model
 # rast = a raster mask to which RGB values are to be mapped
@@ -153,8 +57,13 @@ pcaToRaster <- function(snpPreds, rast, mapCells){
   pca <- prcomp(snpPreds, center=TRUE, scale.=FALSE)
   
   ##assigns to colors, edit as needed to maximize color contrast, etc.
-  a1 <- pca$x[,1]; a2 <- pca$x[,2]; a3 <- pca$x[,3]
-  r <- a1+a2; g <- -a2; b <- a3+a2-a1
+  a1 <- pca$x[,1]
+  a2 <- pca$x[,2]
+  a3 <- pca$x[,3]
+  
+  r <- a1+a2 
+  g <- -a2 
+  b <- a3+a2-a1
   
   ##scales colors
   scalR <- (r-min(r))/(max(r)-min(r))*255
@@ -171,24 +80,193 @@ pcaToRaster <- function(snpPreds, rast, mapCells){
   return(outRast)
 }
 
+#Function 2
+
+# Function to map difference between spatial genetic predictions
+# predMap1 = dataframe of transformed variables from gf or gdm model for first set of SNPs
+# predMap2 = dataframe of transformed variables from gf or gdm model for second set of SNPs
+# rast = a raster mask to which Procrustes residuals are to be mapped
+# mapCells = cell IDs to which Procrustes residuals values should be assigned
+RGBdiffMap <- function(predMap1, predMap2, rast, mapCells){
+  require(vegan)
+  PCA1 <- prcomp(predMap1, center=TRUE, scale.=FALSE)
+  PCA2 <- prcomp(predMap2, center=TRUE, scale.=FALSE)
+  diffProcrust <- procrustes(PCA1, PCA2, scale=TRUE, symmetrical=FALSE)
+  residMap <- residuals(diffProcrust)
+  rast[mapCells] <- residMap
+  return(list(max(residMap), rast))
+}
+
+#Notes from Fitzpatrick & Keller
 # OK, on to mapping. Script assumes:
 # (1) a dataframe named env_trns containing extracted raster data (w/ cell IDs)
 # and env. variables used in the models & with columns as follows: cell, bio1, bio2, etc.
 #
 # (2) a raster mask of the study region to which the RGB data will be written
 
+#############################################################################################
+#Function 3
+
+#Generating a raster stack
+#From Tongli Wang
+
+# x is the wrodkig directory
+# varList: varList <- c('mat','map','td')
+# rType='tif' or 'grid'
+# vConvert=T: 1/10 for mat,mwmt, ...
+rasterStack <- function(x,varList,rType='tif',vConvert=T){
+  library(raster)
+  for(var in varList){
+    if(rType=='tif'){inF <- paste0(x,'/',var,'.tif')}
+    if(rType=='grid'){inF <- paste0(x,'/',var)}
+    r <- raster(inF)
+    if(vConvert==T){
+      if(grepl('mat|MAT|mwmt|MWMT|mcmt|MCMT|td|TD|emt|EMT|ext|EXT             
+                |ahm|AHM|shm|SHM|mar|MAR|ta|Ta|tm|Tm',names(r))){r <- r/10}
+    }
+    if(var==varList[1]){stk=r} else{stk=stack(stk,r)}
+  }
+  return(stk)
+}
+
+#Examples
+#varList <- c('mat','map','td','tmin_sp','ahm','tmax_wt')
+#wd <- 'G:/ClimateXX_out/BC2/BC800v600/Normal_1961_1990MSY'
+#stk <- rasterStack(wd,varList,rType='tif',vConvert=T);stk
+
+
+#############################################################################################
+#############################################################################################
+#Part 1, train the gradient forest model
+
+##Import Data
+snp_clim_bf20 <- read_csv("Genomics_scripts/Data/snp_clim_bayBF20.csv") #pop data
+snp_clim_bf20NA <- read_csv("Genomics_scripts/Data/snp_clim_BF20NA.csv") #pop data
+#snp_clim_ful <- read_csv("Genomics_scripts/Data/snp_clim_full.csv") # full data
+
+#Keep SNP data only
+test_snp <- snp_clim_bf20NA %>% dplyr::select(-Site_Name, -Paper_ID, -Latitude, -Longitude, -Elevation, -MAT, -MAP, -CMD,
+                                       -PAS, -EXT, -Tave_wt, -Tave_sm, -PPT_wt, -PPT_sm)
+
+#Import Raster
+#env_wna <- read_csv("Genomics_scripts/Data/env_wna.csv") #grided WNA climate data
+
+
+## Generate specific dataframes for GF model
+env_site <- snp_clim_bf20 %>% dplyr::select(MAT,MAP,CMD)
+
+
+#Convert to data frame
+test_snp<-as.data.frame(test_snp)
+env_site<-as.data.frame(env_site)
+
+#Merge and make lables
+df_in_1<-data.frame(env_site, test_snp)
+resp<-colnames(test_snp)
+pred<-colnames(env_site)
+
+
+#resp<-as.factor(resp)
+#resp<-droplevels(resp)
+
+###################################################################################
+###################################################################################
+#Run Gradient Forest
+
+# Gradient Forest Model
+#gf <- gradientForest(df_in_1,predictor.vars = pred, 
+#                     response.vars = resp,ntree = 500, 
+#                     transform = NULL, compact = T, nbin = 201 , corr.threshold = 0.5)
+
+
+
+maxLevel <- log2(0.368*nrow(df_in_1)/2) #account for correlations, see ?gradientForest 
+
+# Gradient Forest Model
+gf <- gradientForest(df_in_1,predictor.vars = pred, 
+                     response.vars = resp,ntree = 500, 
+                     maxLevel = maxLevel, trace=T, corr.threshold = 0.5)
+
+#Older version that doesnot work
+#gf <- gradientForest(cbind(env_site, test_snp),predictor.vars = colnames(env_site),
+#                    response.vars = colnames(test_snp),ntree = 500,
+#                    maxLevel=maxLevel, trace=T, corr.threshold=0.50)
+
+#Version from Keller & Fitzpatrick
+#gfRef <- gradientForest(cbind(envGF, SNPs_ref), predictor.vars=colnames(envGF),
+#                        response.vars=colnames(SNPs_ref), ntree=500, 
+#                        maxLevel=maxLevel, trace=T, corr.threshold=0.50)
+
+
+#Importance Plot
+plot(gf)
+
+
+##################################################################################################################
+#Part 2, use gradient forest model on rasters
+
+###################################################################################
+###################################################################################
+## Raster import and manipulation
+
+##Import 1981-2010 raster data for West NA & and stack them
+
+wd <- "C:/Users/anstett3/Documents/Genomics/Large_files/Normal_1981_2010"
+vlist <- c("MAT","MAP","CMD")
+stk <- rasterStack(wd,vlist,rType='tif',vConvert=F)
+
+#Reproject to WGS 1984 (EPSG4326)
+EPSG4326<-"+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" #setup WGS 1984 CRS
+stk <- projectRaster(stk, crs=EPSG4326) #reproject to WGS 1984 (EPSG 4326)
+
+ 
+# Import M.cardinalis ensamble range extent as sf polygon
+c_range <- st_read("SDM/Output/c_range_2.shp") 
+c_range <- st_transform(c_range, crs = 4326) # reproject to WGS 1984 (EPSG 4326)
+raster_range <- 
+
+#Check CRS
+crs(c_range) 
+crs(stk)
+
+#Clip
+stk.clip <- raster::crop(stk, extent(c_range))
+stk.mask <- mask(stk.clip, c_range)
+
+#Extract point from raster stack
+stk.df <- data.frame(rasterToPoints(stk.mask))
+stk.df <- na.omit(stk.df)
+
+#Convert xy coordinates into cell ID
+stk.df.cell<-cellFromXY(stk.mask, cbind(stk.df$x, stk.df$y))
+
+#Get mask for RGB
+MAT.clip <- raster("Donor_selection/data/clip/MAT.clip.grd")
+#MAT.clip <- projectRaster(MAT.clip, crs=EPSG4326) #reproject to WGS 1984 (EPSG 4326)
+rbg_mask <- raster::crop(MAT.clip, extent(c_range))
+rbg_mask <- mask(rbg_mask, c_range)
+rbg_mask <- rbg_mask * 0
+
+###################################################################################
+# Mapping spatial genetic variation --------------------------------------------
+# This is where I start haivng issues
+
+
 # transform env using gf models, see ?predict.gradientForest
-predRef <- predict(gf, env_wna[,-1]) # remove cell column before transforming
+predBF20 <- predict(gf, stk.df[,-1:-2]) # remove cell column before transforming
 
 # map continuous variation 
-refRGBmap <- pcaToRaster(predRef, mask, env_wna$cell)
-plotRGB(refRGBmap)
-writeRaster(refRGBmap, "/.../refSNPs_map.tif", format="GTiff", overwrite=TRUE)
+BF20_RGBmap <- pcaToRaster(predBF20, stk.mask, stk.df.cell)
+plotRGB(BF20_RGBmap)
+writeRaster(BF20_RGBmap, "Offset_graphs/current_climate_BF20_map.tif", format="GTiff", overwrite=TRUE)
 
 
 
 
 ################################################################################
+
+
+
 
 
 # Calculate and map "genetic offset" under climate change ----------------------
@@ -199,14 +277,20 @@ writeRaster(refRGBmap, "/.../refSNPs_map.tif", format="GTiff", overwrite=TRUE)
 # (2) a dataframe named env_trns_future containing extracted raster data of 
 # env. variables for FUTURE a climate scenario, same structure as env_trns
 
+#Import future clmite change raster
+#Clip & Mask
+
 # first transform FUTURE env. variables
-projGI5 <- predict(gfGI5, env_trns_future[,-1])
+#projGI5 <- predict(gfGI5, env_trns_future[,-1])
+projGI5 <- predict(predBF20, env_trns_future[,-1:-2])
 
 # calculate euclidean distance between current and future genetic spaces  
 genOffsetGI5 <- sqrt((projGI5[,1]-predGI5[,1])^2+(projGI5[,2]-predGI5[,2])^2
-                     +(projGI5[,3]-predGI5[,3])^2+(projGI5[,4]-predGI5[,4])^2
-                     +(projGI5[,5]-predGI5[,5])^2+(projGI5[,6]-predGI5[,6])^2
-                     +(projGI5[,7]-predGI5[,7])^2)
+                     +(projGI5[,3]-predGI5[,3])^2)
+                     
+                     #+(projGI5[,4]-predGI5[,4])^2
+                     #+(projGI5[,5]-predGI5[,5])^2+(projGI5[,6]-predGI5[,6])^2
+                     #+(projGI5[,7]-predGI5[,7])^2)
 
 # assign values to raster - can be tricky if current/future climate
 # rasters are not identical in terms of # cells, extent, etc.
@@ -267,9 +351,47 @@ plot(gf, plot.type = "P", show.names = F, horizontal = F, cex.axis = 1, cex.labe
 
 
 
+#MAT.clip <- raster("Donor_selection/data/clip/MAT.clip.grd")
+#MAP.clip <- raster("Donor_selection/data/clip/MAP.clip.grd")
+#PAS.clip <- raster("Donor_selection/data/clip/PAS.clip.grd")
+#EXT.clip <- raster("Donor_selection/data/clip/EXT.clip.grd")
+#CMD.clip <- raster("Donor_selection/data/clip/CMD.clip.grd")
+
+#Seasonal
+#PPT_sm.clip <- raster("Donor_selection/data/clip/PPT_sm.clip.grd")
+#PPT_wt.clip <- raster("Donor_selection/data/clip/PPT_wt.clip.grd")
+#Tave_sm.clip <- raster("Donor_selection/data/clip/Tave_sm.clip.grd")
+#Tave_wt.clip <- raster("Donor_selection/data/clip/Tave_wt.clip.grd")
+
+#Stack Raster
+#env_wna <- stack(list(MAT=MAT.clip,MAP=MAP.clip,CMD=CMD.clip))
+#env_wna <- as.data.frame(env_wna, xy=TRUE)
+
+#colnames(env_wna)[1]<-"X"
+#colnames(env_wna)[2]<-"Y"
+
+# The rasters are for all of North America and too large for storage in this repo
+# Trim them to study area and save only trimmed files in the repo
+# Define extent as 1 degree beyond lat-long extent of points
 
 
 
+
+
+
+
+clim <- read_csv("SDM/data_files/points_Normal_1961_1990MSY.csv")
+prj.laea <- "+proj=laea +lon_0=-100 +lat_0=45 +type=crs" ## set laea CRS
+ext <- extent(min(clim$Longitude)-1, max(clim$Longitude)+1, min(clim$Latitude)-1, max(clim$Latitude)+1)
+bbox = as(ext, "SpatialPolygons") #convert coordinates to a bounding box
+prj.wgs = "+proj=longlat + type=crs" #define unprojected coordinate system
+proj4string(bbox) <- CRS(prj.wgs) #set projection
+bbox.lcc = spTransform(bbox, CRS=CRS(prj.laea)) #re-project to match rasters
+
+
+
+#Now we're going to use the bbox info as the y argument to extract info from the raster
+#env_wna_df <-extract(stk, bbox.lcc,cellnumbers=T)
 
 
 
