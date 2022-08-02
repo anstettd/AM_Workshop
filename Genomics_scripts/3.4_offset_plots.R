deac7539bb66bf25c26c849a94ba121b200b93e5
danstett <daniel_anstett@yahoo.ca>
2022-08-01 22:01:57 -0400

    summary_movements

--- Genomics_scripts/3.4_offset_plots.R
+++ Genomics_scripts/3.4_offset_plots.R
@@ -46,11 +46,17 @@ baseline_pop_sf <- st_as_sf(baseline_pop,coords=c("Long","Lat"), crs=EPSG4326)
 
 ##############################################################################
 
+#Custum pallet
+
+off_pallet <- c("#2166AC","#67A9CF","#D1E5F0","#f7c1c8","#f21836","#A50F15")
+
 #Plot offset SSP245 (RCP 4.5)
 tmap_mode("plot")
 #tmap_mode("view")
 offset45 <- tm_shape(mask_offset_45, bbox=st_bbox(calo)) + #legal boundires
-  tm_raster()+
+  tm_raster(palette = off_pallet)+
+#  tm_raster(palette = rev(brewer.pal(6, "RdBu")))+
+#  tm_raster(palette = "Reds")+
   tm_shape(calo)+
   tm_borders()+
   tm_shape(timeseries_pop_sf)+
@@ -59,12 +65,15 @@ offset45 <- tm_shape(mask_offset_45, bbox=st_bbox(calo)) + #legal boundires
 #  tm_dots(size=0.1,shape=1)+
   tm_layout(legend.position = c(1.03, 0.73),legend.title.size = 0.001)
 offset45
+tmap_save(offset45, filename = "Offset_graphs/offset45_BF20.pdf",width=4, height=7)
+
+off_pallet2 <- c("#2166AC","#67A9CF","#D1E5F0","#f7c1c8","#f21836","#A50F15","#5c0915")
 
 #Plot offset SSP585 (RCP 8.5)
 tmap_mode("plot")
 #tmap_mode("view")
 offset85 <- tm_shape(mask_offset_85, bbox=st_bbox(calo)) + #legal boundires
-  tm_raster()+
+  tm_raster(palette = off_pallet2)+
   tm_shape(calo)+
   tm_borders()+
   tm_shape(timeseries_pop_sf)+
@@ -73,4 +82,6 @@ offset85 <- tm_shape(mask_offset_85, bbox=st_bbox(calo)) + #legal boundires
   #  tm_dots(size=0.1,shape=1)+
   tm_layout(legend.position = c(1.03, 0.73),legend.title.size = 0.001)
 offset85
+tmap_save(offset85, filename = "Offset_graphs/offset85_BF20.pdf",width=4, height=7)
+
 
