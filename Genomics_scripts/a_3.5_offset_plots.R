20f2215c99c5d928270bdf0d5da5e87b285fe2e3
danstett <daniel_anstett@yahoo.ca>
2022-09-20 14:42:26 -0400

    ggc offset plots

--- Genomics_scripts/a_3.5_offset_plots.R
+++ Genomics_scripts/a_3.5_offset_plots.R
@@ -24,20 +24,23 @@ library(RColorBrewer)
 
 ## INPUTS
 
-#Raster
-mask_offset_2016 <- raster("Genomics_scripts/Data/offset_2016.tif") #pop data
+#Timeseries offset raster
+#mask_offset_2016 <- raster("Genomics_scripts/Data/offset_2016.tif") #pop data
+
+#Future cliamte change offset raster
 mask_offset_45 <- raster("Genomics_scripts/Data/offset_4.5_peakbf2.tif") #pop data
 mask_offset_85 <- raster("Genomics_scripts/Data/offset_8.5_peakbf2.tif") #pop data
+
 
 #Define CRS
 EPSG4326<-"+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" #setup WGS 1984 CRS
 
 #Mask by M. caridnalis range extent
-c_range <- st_read("SDM/Output/c_range_2.shp") 
-c_range <- st_transform(c_range, crs = 4326) # reproject to WGS 1984 (EPSG 4326)
-mask_offset_2016 <- mask(mask_offset_2016, c_range)
-mask_offset_45 <- mask(mask_offset_45, c_range)
-mask_offset_85 <- mask(mask_offset_85, c_range)
+#c_range <- st_read("SDM/Output/c_range_2.shp") 
+#c_range <- st_transform(c_range, crs = 4326) # reproject to WGS 1984 (EPSG 4326)
+#mask_offset_2016 <- mask(mask_offset_2016, c_range)
+#mask_offset_45 <- mask(mask_offset_45, c_range)
+#mask_offset_85 <- mask(mask_offset_85, c_range)
 
 
 # California & Oregon Map Setup
@@ -77,7 +80,7 @@ offset_2016
 
 ##############################################################################
 
-#Custum pallet
+#Future climate change
 
 off_pallet <- c("#2166AC","#67A9CF","#D1E5F0","#f7c1c8","#f21836","#A50F15")
 
@@ -91,28 +94,29 @@ offset45 <- tm_shape(mask_offset_45, bbox=st_bbox(calo)) + #legal boundires
   tm_shape(calo)+
   tm_borders()+
   tm_shape(timeseries_pop_sf)+
-  tm_dots(size=0.2,shape=1)+
+  tm_dots(size=0.5,shape=1,col="black",border.lwd = 2.5)+
 #  tm_shape(baseline_pop_sf)+
 #  tm_dots(size=0.1,shape=1)+
-  tm_layout(legend.position = c(1.03, 0.73),legend.title.size = 0.001)
+  tm_layout(legend.position = c(0.62, 0.48),legend.title.size = 0.001)
 offset45
-#tmap_save(offset45, filename = "Offset_graphs/offset45_peakbf2.pdf",width=4, height=7)
+tmap_save(offset45, filename = "Offset_graphs/offset45_peakbf2.pdf",width=4, height=7)
 
 off_pallet2 <- c("#2166AC","#67A9CF","#D1E5F0","#f7c1c8","#f21836","#A50F15","#5c0915")
+off_pallet3 <- c("#2166AC","#67A9CF","#D1E5F0","#f7c1c8","#f21836","#A50F15")
 
 #Plot offset SSP585 (RCP 8.5)
 tmap_mode("plot")
 #tmap_mode("view")
 offset85 <- tm_shape(mask_offset_85, bbox=st_bbox(calo)) + #legal boundires
-  tm_raster(palette = off_pallet2)+
+  tm_raster(palette = off_pallet3)+
   tm_shape(calo)+
   tm_borders()+
   tm_shape(timeseries_pop_sf)+
-  tm_dots(size=0.2,shape=1)+
+  tm_dots(size=0.5,shape=1,col="black",border.lwd = 2.5)+
   #  tm_shape(baseline_pop_sf)+
   #  tm_dots(size=0.1,shape=1)+
-  tm_layout(legend.position = c(1.03, 0.73),legend.title.size = 0.001)
+  tm_layout(legend.position = c(0.62, 0.48),legend.title.size = 0.001)
 offset85
-#tmap_save(offset85, filename = "Offset_graphs/offset85_peakbf2.pdf",width=4, height=7)
+tmap_save(offset85, filename = "Offset_graphs/offset85_peakbf2.pdf",width=4, height=7)
 
 
