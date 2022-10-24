7ba886a9a7d19be817c719177861f809f1a30f40
Anstett <anstett3@msu.edu>
2022-10-24 13:39:14 -0400

    update SNP prep GF to 5

--- Genomics_scripts/a_3.2_get_climate_SNPs.R
+++ Genomics_scripts/a_3.2_get_climate_SNPs.R
@@ -14,9 +14,9 @@ library(tidyverse)
 climate <- read_csv("Donor_selection/Data/climate_pop.csv")
 
 #Import peak bf>2 SNP data for each environmental variable
-env1_peakbf2 <- read_csv("Genomics_scripts/Data/win_bf_mat.csv")
-env2_peakbf2 <- read_csv("Genomics_scripts/Data/win_bf_map.csv")
-env5_peakbf2 <- read_csv("Genomics_scripts/Data/win_bf_cmd.csv")
+env1_peakbf2 <- read_csv("Genomics_scripts/Data/win_bf_mat30_5.csv")
+env2_peakbf2 <- read_csv("Genomics_scripts/Data/win_bf_map30_5.csv")
+env5_peakbf2 <- read_csv("Genomics_scripts/Data/win_bf_cmd30_5.csv")
 
 #Merge into 1 dataframe that includes every unique allele
 env_merge <- rbind(env1_peakbf2,env2_peakbf2,env5_peakbf2)
@@ -47,11 +47,11 @@ for (i in seq (2,dim(loci_env)[2]-1,2)){
 rownames(snp_bay)<- loci_env$chr_snp
 snp_bay_T <- as.data.frame(t(snp_bay))
 snp_clim_bayNA <- cbind(climate,snp_bay_T)
-write_csv(snp_clim_bayNA, "Genomics_scripts/Data/snp_clim_peakbf2NA.csv")
+write_csv(snp_clim_bayNA, "Genomics_scripts/Data/snp_clim_peakbf5NA.csv")
 
 #Remove NA's
 snp_clim_bay_noNA <- snp_clim_bayNA %>% select_if(~ !any(is.na(.)))
-write_csv(snp_clim_bay_noNA, "Genomics_scripts/Data/snp_clim_peakbf2_noNA.csv")
+write_csv(snp_clim_bay_noNA, "Genomics_scripts/Data/snp_clim_peakbf5_noNA.csv")
 
 
 #Generate snpA table for full baseline snp datatset #Does not run locally
