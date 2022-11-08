605addfaf686a869ff66d5d9612607f3a5f84dbe
danstett <daniel_anstett@yahoo.ca>
2022-11-08 14:54:47 -0500

    slope density

---
+++ Genomics_scripts/a_2.5_slope_density.R
@@ -0,0 +1,283 @@
+##################################################################################
+## Plot Slopes Against BF and Window
+## 
+## Author Daniel Anstett
+## 
+## Last Modified October 12, 2022
+###################################################################################
+library(tidyverse)
+
+#Import Slopes
+slope_mat <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/slope_mat.csv")
+#slope_map <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/slope_map.csv")
+#slope_cmd <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/slope_cmd.csv")
+slope_mat <- slope_mat %>% mutate(log_p=-log(approx_p,base=10))
+slope_mat25 <- slope_mat %>% filter(Slope>-25) %>% filter(Slope<25)
+slope_mat15 <- slope_mat %>% filter(Slope>-15) %>% filter(Slope<15)
+slope_mat5 <- slope_mat %>% filter(Slope>-5) %>% filter(Slope<5)
+slope_mat2 <- slope_mat %>% filter(Slope>-2) %>% filter(Slope<2)
+
+
+#Make Sites
+slope_mat_p1 <- slope_mat %>% filter(Site==1)
+slope_mat2_p1 <- slope_mat2 %>% filter(Site==1)
+
+slope_mat_bf0_p1 <- slope_mat %>% filter(Site==1) %>% filter(BF>0)
+slope_mat2_bf0_p1 <- slope_mat2 %>% filter(Site==1) %>% filter(BF>0)
+
+slope_mat_bf10_p1 <- slope_mat %>% filter(Site==1) %>% filter(BF>10)
+slope_mat2_bf10_p1 <- slope_mat2 %>% filter(Site==1) %>% filter(BF>10)
+
+
+
+
+###################################################################################
+#Test with P1
+
+###Slope =  -2 to 2
+
+#BF > 10
+
+slope_bf2 <- ggplot(slope_mat2_bf10_p1, aes(x=Slope, y=BF)) + 
+  geom_hex() +
+  #geom_bin2d()
+  #geom_density_2d()+
+  #stat_density_2d(aes(fill = ..level..), geom = "polygon")+
+  #stat_density_2d(aes(fill = ..level..), geom = "polygon", colour="white")
+  #stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE) +
+  scale_y_continuous(name="Log Bayes Factor")+
+  scale_x_continuous(name="Slope")+
+  scale_fill_viridis_c()+
+  #,limits=c(-25,25))+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))
+slope_bf2
+#+
+#  facet_wrap(~Site)
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/mat_density_2_bf10_p1.png",
+       plot=slope_bf2, width=8, height = 6, units = "in")
+
+#BF > 0
+
+slope_bf2 <- ggplot(slope_mat2_bf0_p1, aes(x=Slope, y=BF)) + 
+  geom_hex() +
+  #geom_bin2d()
+  #geom_density_2d()+
+  #stat_density_2d(aes(fill = ..level..), geom = "polygon")+
+  #stat_density_2d(aes(fill = ..level..), geom = "polygon", colour="white")
+  #stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE) +
+  scale_y_continuous(name="Log Bayes Factor")+
+  scale_x_continuous(name="Slope")+
+  scale_fill_viridis_c()+
+#,limits=c(-25,25))+
+#scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+theme_classic() + theme(
+  axis.text.x = element_text(size=14, face="bold"),
+  axis.text.y = element_text(size=14,face="bold"),
+  axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+  axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))
+slope_bf2
+#+
+#  facet_wrap(~Site)
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/mat_density_2_bf0_p1.png",
+       plot=slope_bf2, width=8, height = 6, units = "in")
+
+
+
+#BF all
+
+slope_bf2 <- ggplot(slope_mat2_p1, aes(x=Slope, y=BF)) + 
+  geom_hex() +
+  #geom_bin2d()
+  #geom_density_2d()+
+  #stat_density_2d(aes(fill = ..level..), geom = "polygon")+
+  #stat_density_2d(aes(fill = ..level..), geom = "polygon", colour="white")
+  #stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE) +
+  scale_y_continuous(name="Log Bayes Factor")+
+  scale_x_continuous(name="Slope")+
+  scale_fill_viridis_c()+
+  #,limits=c(-25,25))+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))
+#slope_bf2
+#+
+#  facet_wrap(~Site)
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/mat_density_2_p1.png",
+       plot=slope_bf2, width=8, height = 6, units = "in")
+
+
+
+
+#BF All range
+
+slope_bf <- ggplot(slope_mat_p1, aes(x=Slope, y=BF)) + 
+  #geom_point(aes(), size = 0.2)+
+  geom_hex() +
+  #geom_bin2d() +
+  scale_y_continuous(name="Log Bayes Factor",breaks=seq(-40,40,20))+
+  scale_x_continuous(name="Slope")+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))
+#+
+#  facet_wrap(~Site)
+
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/mat_density_50_p1.png",
+       plot=slope_bf, width=8, height = 6, units = "in")
+
+
+
+
+###################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#BF -25 to 25
+slope_bf25 <- ggplot(slope_mat25, aes(x=Slope, y=BF)) + 
+#  geom_point(aes(), size = 0.2)+
+  geom_hex() +
+  scale_y_continuous(name="Log Bayes Factor",breaks=seq(-40,40,20))+
+  scale_x_continuous(name="Slope")+
+  #,limits=c(-25,25))+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))
+  #+
+  #  facet_wrap(~Site)
+
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/mat_density_25_p1.png",
+       plot=slope_bf25, width=8, height = 6, units = "in")
+
+#BF -5 to 5
+slope_bf5 <- ggplot(slope_mat5, aes(x=Slope, y=BF)) + 
+  geom_point(aes(), size = 0.2)+
+  geom_hex() +
+  scale_y_continuous(name="Log Bayes Factor",breaks=seq(-40,40,20))+
+  scale_x_continuous(name="Slope")+
+  #,limits=c(-25,25))+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))
+  #+
+  #  facet_wrap(~Site)
+
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/mat_density_5_p1.png",
+       plot=slope_bf5, width=8, height = 6, units = "in")
+
+
+
+
+
+
+###################################################################################
+
+#Window
+
+#log_p All range
+
+slope_log_p <- ggplot(slope_mat, aes(x=Slope, y=log_p)) + 
+  geom_point(aes(), size = 0.2)+
+  #geom_smooth(method=lm,color="black")+
+  #  geom_label_repel(aes(label = Paper_ID))+
+#  scale_y_continuous(name="Log Bayes Factor",breaks=seq(-40,40,20))+
+  scale_x_continuous(name="Slope")+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))+
+  facet_wrap(~Site)
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/slope_log_p_mat_facet50.png",
+       plot=slope_log_p, width=8, height = 6, units = "in")
+
+#log_p -25 to 25
+slope_log_p25 <- ggplot(slope_mat25, aes(x=Slope, y=log_p)) + 
+  geom_point(aes(), size = 0.2)+
+  #geom_smooth(method=lm,color="black")+
+  #  geom_label_repel(aes(label = Paper_ID))+
+#  scale_y_continuous(name="Log Bayes Factor",breaks=seq(-40,40,20))+
+  scale_x_continuous(name="Slope")+
+  #,limits=c(-25,25))+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))+
+  facet_wrap(~Site)
+
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/slope_log_p_mat_facet25.png",
+       plot=slope_log_p25, width=8, height = 6, units = "in")
+
+#log_p -5 to 5
+slope_log_p5 <- ggplot(slope_mat5, aes(x=Slope, y=log_p)) + 
+  geom_point(aes(), size = 0.2)+
+  #geom_smooth(method=lm,color="black")+
+  #  geom_label_repel(aes(label = Paper_ID))+
+  #scale_y_continuous(name="Log Bayes Factor",breaks=seq(-40,40,20))+
+  scale_x_continuous(name="Slope")+
+  #,limits=c(-25,25))+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))+
+  facet_wrap(~Site)
+
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/slope_log_p_mat_facet5.png",
+       plot=slope_log_p5, width=8, height = 6, units = "in")
+
+#log_p -2 to 2
+slope_log_p2 <- ggplot(slope_mat2, aes(x=Slope, y=log_p)) + 
+  geom_point(aes(), size = 0.2)+
+  #geom_smooth(method=lm,color="black")+
+  #  geom_label_repel(aes(label = Paper_ID))+
+  #scale_y_continuous(name="Log Bayes Factor",breaks=seq(-40,40,20))+
+  scale_x_continuous(name="Slope")+
+  #,limits=c(-25,25))+
+  #scale_color_manual(values= c("North"="#3399FF", "Centre"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5))+
+  facet_wrap(~Site)
+
+ggsave("/Users/daniel_anstett/Dropbox/AM_Workshop/large_graphs/slope_log_p_mat_facet2.png",
+       plot=slope_log_p2, width=8, height = 6, units = "in")
+
